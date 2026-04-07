e5ec6f1fc0c6b58da1792928515c8908f995b87b
LaraLizx <laramacedodeliz@gmail.com>
2026-04-07 14:18:35 -0300

    Update CursoTecnico.java

--- mavenproject1/src/main/java/Lara/CursoTecnico.java
+++ mavenproject1/src/main/java/Lara/CursoTecnico.java
@@ -13,10 +13,32 @@ public abstract class CursoTecnico extends Curso {
     private String eixoTecnologico;
     private int chEstagio;
 
+    public String geteixoTecnologico(){
+        return eixoTecnologico;
+    }
+    public void seteixoTecnologico (String eixoTecnologico){
+        this.eixoTecnologico = eixoTecnologico;
+    }
+    public int getchEstagio(){
+        return chEstagio;
+    }
+    public void setchEstagio (int chEstagio){
+        this.chEtagio
+    }
+
+    public void verificarEstagio(){
+        //verificar estagio
+    }
+    
     public boolean estagioObrigatorio() {
         return chEstagio > 0;
     }
 
+    @Override
+    public void realizarMatricula(){
+        
+    }
+    
     @Override
     public void obterDisciplinas() {
         System.out.println("Disciplinas do curso técnico");
